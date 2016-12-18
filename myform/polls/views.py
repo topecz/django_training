d2b017c54b390a712a409278d4e21c32ed435400
Tomás Pereira Cruz <topec@percar.com>
2016-12-18 23:24:05 +0100

    Tutorial 03, phase two, rewriten

--- myform/polls/views.py
+++ myform/polls/views.py
@@ -1,18 +1,15 @@
 """ step one
-"""
 from django.http import HttpResponse
 from django.template import loader
 """
 from django.http import Http404
 from django.shortcuts import render
 
-"""
 from .models import Question
 
 def index(request):
     latest_question_list    = Question.objects.order_by('-pub_date')[:5]
     """ step one
-    """
     template                = loader.get_template('polls/index.html')
     context                 = { 'latest_question_list': latest_question_list, }
     return HttpResponse(template.render(context, request))
@@ -20,7 +17,6 @@ def index(request):
     # shortcut
     context     = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
-    """
 
 def detail(request, question_id):
     try:
