959df64cf1ca774cbc473ef2d955760dee120872
Tomás Pereira Cruz <topec@percar.com>
2016-12-18 23:31:28 +0100

    Tutorial 03, using the template system

--- myform/polls/views.py
+++ myform/polls/views.py
@@ -1,9 +1,11 @@
 """ step one
 from django.http import HttpResponse
 from django.template import loader
-"""
+# step two
 from django.http import Http404
 from django.shortcuts import render
+"""
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -19,10 +21,13 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
+    """ step two
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
+    """
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
