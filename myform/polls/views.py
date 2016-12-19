4a93b588e2e3ba871dc9c433f7d26bcda54b21c5
Tomás Pereira Cruz <topec@percar.com>
2016-12-19 20:18:25 +0100

    Tutorial 04, Write a simple form

--- myform/polls/views.py
+++ myform/polls/views.py
@@ -6,8 +6,10 @@ from django.http import Http404
 from django.shortcuts import render
 """
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Question
 
 def index(request):
     latest_question_list    = Question.objects.order_by('-pub_date')[:5]
@@ -31,8 +33,26 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    reponse = "You're looking at the results of question %s."
-    return HttpResponse(reponse % question_id)
+    #reponse = "You're looking at the results of question %s."
+    #return HttpResponse(reponse % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're looking on question %s." % question_id)
+    #return HttpResponse("You're looking on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choise.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after succesfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
